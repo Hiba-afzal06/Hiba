df1e49a6644403bdfaf200877c12c71126ec94e9
hiba.naaz <hiba.naaz@ZIL482.perficient.com>
2023-09-21 17:44:47 +0530

    Commit Test Suite

---
+++ Test Suites/TestSuite_002.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_002</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1c366b43-9869-4d88-92b6-e2c00e307bd2</testSuiteGuid>
+</TestSuiteEntity>
